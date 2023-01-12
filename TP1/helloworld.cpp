e0d3e1659846ca4e2bb77982deb0e477f30b6b55
bastienlagier <bastien.lagier@edu.univ-eiffel.fr>
2023-01-12 15:04:25 +0100

    Added Hello <name>

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    int var = 0;
-    std::cout << "Hello world!" << std::endl;
-    std::cin >> var;;
-    return var;
+    auto name = std::string();
+    std::cout << "What's your name?" << std::endl;
+    std::cin >> name;
+    std::cout << "Hello " <<  name << std::endl;
+    return 0;
 }
