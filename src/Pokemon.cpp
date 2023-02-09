22fb68919c94b64d7d6d87dfe9dbbb2469053cb2
bastienlagier <bastien.lagier@edu.univ-eiffel.fr>
2023-02-09 15:22:05 +0100

    Added Pokemon

--- src/Pokemon.cpp
+++ src/Pokemon.cpp
@@ -1,1 +1,20 @@
 #include "Pokemon.h"
+#include <string>
+
+
+std::string Pokemon::name() const {
+    return _name;
+}
+
+unsigned int Pokemon::id() const {
+    return _id;
+}
+
+Pokemon& Pokemon::operator=(const Pokemon& other) {
+    if (this != &other) {
+        _name = other._name;
+    }
+    return *this;
+}
+
+unsigned int Pokemon::idPokemon = 0;
--- src/Pokemon.h
+++ src/Pokemon.h
@@ -2,6 +2,29 @@
 
 #include <string>
 
+
 // A creature that is cute and can fight other ones.
 class Pokemon
-{};
+{
+    private:
+    std::string _name;
+    unsigned int _id;
+
+    public:
+    Pokemon(const std::string& name)
+        : _name { name }
+    {
+        _id = idPokemon++;
+    }
+
+    Pokemon(const Pokemon& other)
+        : _name { other._name }
+        , _id { idPokemon++ }
+    {}
+
+    Pokemon& operator=(const Pokemon& other);
+
+    std::string name() const;
+    unsigned int id() const;
+    static unsigned int idPokemon;
+};
