39df101e4d6735c32350c048137da9272a3f22ac
bastienlagier <bastien.lagier@edu.univ-eiffel.fr>
2023-02-14 10:46:02 +0100

    Added Trap

--- src/Trap.hpp
+++ src/Trap.hpp
@@ -1,1 +1,42 @@
 #pragma once
+
+#include <string>
+#include "Card.hpp"
+using namespace std;
+
+enum class TrapType {
+    Normal,
+    Continuous,
+    Counter
+};
+
+string to_string(TrapType traptype) {
+    switch (traptype) {
+        case TrapType::Normal:
+        return "Normal";
+        case TrapType::Continuous:
+        return "Continuous";
+        case TrapType::Counter:
+        return "Counter";
+        default:
+        return "";
+    }
+};
+
+class Trap : public Card {
+    public:
+    Trap(const string& id, const string& name, TrapType traptype)
+        : Card { id, CardType::Trap }
+        , _id { id }
+        , _traptype { traptype }
+    {
+        Card::set_name(name);
+    }
+    TrapType get_trap_type() const {
+        return _traptype;
+    }
+
+    private:
+    const string _id;
+    const TrapType _traptype;
+};
