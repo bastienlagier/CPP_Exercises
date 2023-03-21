8afd144232a0ef037efecae6a5121d3206fa07a4
bastienlagier <bastien.lagier@edu.univ-eiffel.fr>
2023-03-21 13:58:12 +0100

    Added last changes [vscode bug]

--- src/Kitchen.cpp
+++ src/Kitchen.cpp
@@ -1,1 +1,14 @@
 #include "Kitchen.hpp"
+
+const Unit& Kitchen::register_unit(Unit unit) {
+    return _array.emplace_back(unit);
+}
+
+const Unit* Kitchen::find_unit(const std::string& name) const {
+    auto lambda = [name](const Unit& u) { return u.name == name; };
+    auto unit = std::find_if(std::begin(_array), std::end(_array), lambda); 
+    if (unit != std::end(_array)) {
+        return &(*unit);
+    }
+    return nullptr;
+}
--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,16 @@
 #pragma once
+#include "../lib/Unit.hpp"
+#include <vector>
+#include <memory>
+#include <utility>
+#include <algorithm>
 
 class Kitchen
-{};
+{
+    public:
+    const Unit& register_unit(Unit unit);
+    const Unit* find_unit(const std::string& name) const;
+
+    private:
+    std::vector<Unit> _array;
+};
