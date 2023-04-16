319fa791585d61fc28e6932c87248ad7d196a69f
bastien.lagier <bastien.lagier@edu.univ-eiffel.fr>
2023-04-16 20:07:39 +0200

    Test01 to Test02 working

--- src/Kitchen.cpp
+++ src/Kitchen.cpp
@@ -1,1 +1,28 @@
 #include "Kitchen.hpp"
+
+
+const Unit& Kitchen::register_unit(Unit unit) {
+    return _units.emplace_back(unit);
+}
+
+const Unit* Kitchen::find_unit(const std::string& name) const {
+    const auto it = std::find_if(_units.begin(), _units.end(), [name](Unit unit) { return unit.name == name ; });
+    return it == _units.end() ? nullptr : &((*it).get());
+}
+
+const Ingredient& Kitchen::register_ingredient(Ingredient ingredient) {
+    return _ingredients.emplace_back(ingredient);
+}
+
+std::string to_upper(std::string s) {
+    std::transform(s.begin(), s.end(), s.begin(), 
+                   [](unsigned char c){ return std::toupper(c); }
+    );
+    return s;
+}
+
+const Ingredient* Kitchen::find_ingredient(const std::string& name) const {
+    const auto it = std::find_if(_ingredients.begin(), _ingredients.end(), 
+        [name](Ingredient i) { return to_upper(i.name) == to_upper(name); });
+    return it == _ingredients.end() ? nullptr : &((*it).get());
+}
--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,25 @@
 #pragma once
 
+#include <vector>
+#include <list>
+#include <iostream>
+#include <memory>
+#include <algorithm>
+
+#include "../lib/Unit.hpp"
+#include "../lib/Ingredient.hpp"
+
 class Kitchen
-{};
+{
+    public:
+    const Unit& register_unit(Unit unit);
+    const Unit* find_unit(const std::string& name) const;
+
+
+    const Ingredient& register_ingredient(Ingredient ingredient);
+    const Ingredient* find_ingredient(const std::string& name) const;
+
+    private:
+    std::vector<std::reference_wrapper<Unit>> _units;
+    std::vector<std::reference_wrapper<Ingredient>> _ingredients;
+};
